d35ff3dd84ba6ffbc3d36400ca400fea6a1c04ee
akshaypatil8162 <69005473+akshaypatil8162@users.noreply.github.com>
2023-04-19 01:15:10 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
        description = "The AWS region to create things in." 
-       default     = "ap-south-1" 
+       default     = "ap-northeast-1a" 
 }
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "myJune2021Key" 
+    default     =  "T16" 
 }
 
 variable "instance_type" { 
